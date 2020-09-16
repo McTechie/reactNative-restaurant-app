55ee7dfdff057acfb3533cd575abb28b43de5713
Mcvean Soans <mc.soans02@gmail.com>
2020-09-16 16:21:01 +0530

    Deletion of Favorites made available

--- redux/leaders.js
+++ redux/leaders.js
@@ -3,6 +3,7 @@ import * as ActionTypes from './ActionTypes';
 export const leaders = (state  = { isLoading: true, errMess: null, leaders:[]}, action) => {
 
     switch (action.type) {
+      
         case ActionTypes.ADD_LEADERS:
             return {...state, isLoading: false, errMess: null, leaders: action.payload};
 
