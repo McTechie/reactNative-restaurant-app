f97fcd1f291b642bdfed4c2b0d3ba1286bc0a12d
Mcvean Soans <mc.soans02@gmail.com>
2020-09-17 12:12:54 +0530

    Updated Android Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -32,7 +32,14 @@ class Reservation extends Component {
             if (permission.status !== 'granted') {
                 Alert.alert('Permission not granted to show notifications');
             }
+        } else {
+
+            if (Platform.OS === 'android') {
+              Notifications.createChannelAndroidAsync('notify', { name: 'notify', sound: true, vibrate: true, });
+            }
+
         }
+
         return permission;
     }
 
@@ -45,8 +52,7 @@ class Reservation extends Component {
                 sound: true
             },
             android: {
-                sound: true,
-                vibrate: true,
+                "channelId": "notify",
                 color: '#512DA8'
             }
         });
